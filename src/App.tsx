bf98b13c16359b27c7652f69e00c0a47a5111d97
KrivovDima <krivovdima89@gmail.com>
2021-09-11 17:38:29 +0400

    refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import DisplayCounter from "./components/DisplayCounter";
 import Buttons from "./components/Buttons";
@@ -29,31 +29,6 @@ function App() {
 
   const dispatch = useDispatch()
 
-
-  // let [minValue, setMinValue] = useState<number | ''>(getSettings().minValue);
-  // let [maxValue, setMaxValue] = useState<number | null>(getSettings().maxValue);
-  // let [correctSettings, setCorrectSettings] = useState(getSettings().save);
-  // let [count, setCount] = useState(minValue);
-  //
-  // function getSettings() {
-  //   const saveSettings = localStorage.getItem('settings');
-  //   if (saveSettings) {
-  //     return JSON.parse(saveSettings);
-  //   } else {
-  //     return {
-  //       minValue: '',
-  //       maxValue: null,
-  //       save: false
-  //     }
-  //
-  //   }
-  // }
-  //
-  // const saveSettingsLocalStorage = (minValue: number, maxValue: number) => {
-  //   const settings = {minValue, maxValue, save: true};
-  //   localStorage.setItem('settings', JSON.stringify(settings));
-  // }
-  //
   const changeMinValue = (value: number) => {
     dispatch(changeMinValueAC(value))
   }
@@ -78,36 +53,6 @@ function App() {
     dispatch(addSettingsAC())
   }
 
-
-  //
-  // let errorMin = false;
-  // let errorMax = false;
-  // if (minValue !== null && minValue <= 0) {
-  //   errorMin = true
-  // }
-  // if (maxValue !== null && maxValue <= 0) {
-  //   errorMax = true
-  // }
-  // if (minValue !== null && maxValue !== null && minValue >= maxValue) {
-  //   errorMin = true
-  //   errorMax = true
-  // }
-  //
-  // const addSettings = () => {
-  //   if (!errorMin && !errorMax && minValue && maxValue) {
-  //     setCorrectSettings(true)
-  //     setCount(minValue);
-  //     saveSettingsLocalStorage(minValue, maxValue);
-  //   } else return
-  // }
-  //
-  // function changeSettings(value: boolean) {
-  //   setCorrectSettings(value);
-  // }
-  //
-  // const tripConditionSet = (errorMin || errorMax) || (!minValue || !maxValue) || correctSettings;
-
-
   return (
     <div className="App">
       <div className="set">
@@ -125,11 +70,6 @@ function App() {
              classNameTitle={`btn btnSet`}
              disabled={(errorMin || errorMax) || isDisabledSet}
              clickFn={addSettings}/>
-        <button onClick={() => {
-          localStorage.clear()
-        }}>resetLS
-        </button>
-
       </div>
       <div className="counter">
         <DisplayCounter count={count}
